b030f455527799308431f0df1906564668409b75
traunmus <s2310237027@fhooe.at>
2024-04-24 10:48:44 +0200

    upsala

---
+++ schokoheisshunger_traunmus_android/dummy_proj/dummy.java
@@ -0,0 +1,16 @@
+
+/**
+ * dummy
+ */
+public class dummy {
+    
+	public double getCosts() {
+		Comparable<AbstractMember>[] memberArr = members.toArray(true);
+		double costs = 0;
+		for (int i = 0; i < memberArr.length; i++) {
+			costs += ((AbstractMember) memberArr[i]).getCosts();
+		}
+		return costs;
+	}
+    
+}
